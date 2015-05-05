e635e85e89e932d7f5594e00518561c8947e970d
Lisyk <sergey.liskovoy@gmail.com>
2015-05-05 09:14:28 -0400

    add player name, deck and cards

--- main.rb
+++ main.rb
@@ -2,12 +2,35 @@ require 'rubygems'
 require 'sinatra'
 require 'pry'
 
+set :sessions, true
 
 get '/' do
-  "Hello Sinatra app!"
+  if session[:player_name]
+    redirect '/game'
+
+  else
+    redirect '/new_player'
+  end
 end
 
-get '/test' do
-  "From testing action!" + params[:name].to_s
-  binding.pry
+get '/new_player' do
+  erb :new_player
+end
+
+post 'nem_player' do
+  session[:player_name]=params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  erb :game
 end
